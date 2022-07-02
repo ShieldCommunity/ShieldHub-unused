299485690c5127e06e9f44d894470b7b03fa9044
xIsm4 <minelatinsoporte@gmail.com>
2022-07-02 19:56:26 +0200

    feat: Add command, interface and listeners logic.

--- plugin/src/main/java/com/xism4/shieldhub/ShieldHub.java
+++ plugin/src/main/java/com/xism4/shieldhub/ShieldHub.java
@@ -1,12 +1,24 @@
 package com.xism4.shieldhub;
 
-public class ShieldHub {
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ShieldHub extends JavaPlugin {
 	
     @Override
     public void onEnable() {
+        getLogger().info("ShieldHub has been enabled "+
+                Bukkit.getVersion());
+        commandHandler();
+        eventHandler();
     }
 
     @Override
     public void onDisable() {
+        getLogger().warning("ShieldHub has been disabled, remember to clear data");
     }
+
+    public void commandHandler() {}
+
+    public void eventHandler() {}
 }
