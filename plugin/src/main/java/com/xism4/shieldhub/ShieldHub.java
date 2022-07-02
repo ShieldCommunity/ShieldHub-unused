07f19eb59b95840aacb60a7dab65c39dfa7a7b02
xIsm4 <minelatinsoporte@gmail.com>
2022-07-02 19:25:05 +0200

    feat: Create plugin logic

---
+++ plugin/src/main/java/com/xism4/shieldhub/ShieldHub.java
@@ -0,0 +1,12 @@
+package com.xism4.shieldhub;
+
+public class ShieldHub {
+	
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "ShieldHub"
+
+include("plugin")
